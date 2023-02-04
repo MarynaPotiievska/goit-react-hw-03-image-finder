1d3be1f73a57752682718bb3f3f6aae0785a3bc2
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-02-04 15:00:19 +0200

    added function for http request

---
+++ src/components/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const fetchImages = async (request, page) => {
+	axios.defaults.baseURL = 'https://pixabay.com/';
+	const API_KEY = '27227070-2daccb9679d1d87f210af339a';
+	const resp = await axios.get('api/', {params: {
+		q: request,
+		page,
+		key: API_KEY,
+		image_type: 'photo',
+		orientation: 'horizontal',
+		per_page: 12
+	}
+	});
+	console.log(resp.hits);
+	return resp.hits
+}
